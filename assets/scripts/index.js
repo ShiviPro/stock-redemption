9219f1f5ec6c34dbe84c1173660a1040a630ec29
Shivam Tewari <www.shivamtewari@gmail.com>
2021-08-16 00:44:06 +0530

    style: added themes & emojis for >=50%  change.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,6 +11,11 @@ output.classList.add("output");
 
 const checkBtn = document.querySelector("#check-btn");
 checkBtn.addEventListener("click", (event) => {
+  document.body.classList.remove("body--danger");
+  document.body.classList.remove("body--warning");
+  document.body.classList.remove("body--happy");
+  document.body.classList.remove("body--smile");
+  document.body.classList.remove("body--no-feelings");
   let costPrice = costPriceElem.value;
   let quantity = quantityElem.value;
   let sellingPrice = sellingPriceElem.value;
@@ -33,9 +38,9 @@ checkBtn.addEventListener("click", (event) => {
       let profit = totalSP - totalCP;
       let profitPercent = (profit / totalCP) * 100;
 
-      let congratsMessage25 = "Hey you might be good at this !";
-      let congratsMessage50 = "Woah this feeling is magical !";
-      let congratsMessage75 = "What the !! This might be off all the charts.";
+      let congratsMessage25 = `Hey you might be good at this <i class="fas fa-smile-wink emoji"></i>`;
+      let congratsMessage50 = `Woah this feeling is magical <i class="fas fa-grin-stars emoji"></i>`;
+      let congratsMessage75 = `What the !! This might be off all the charts <i class="fas fa-surprise emoji"></i>`;
 
       output.innerHTML = `
             <h2>Oh you made a profit of ${profitPercent}%, which sums up to a total of ₹${profit}.</h2>
@@ -44,10 +49,12 @@ checkBtn.addEventListener("click", (event) => {
         output.innerHTML += `
             <h2>${congratsMessage75}</h2>
             `;
+        document.body.classList.add("body--happy");
       } else if (profitPercent >= 50) {
         output.innerHTML += `
             <h2>${congratsMessage50}</h2>
             `;
+        document.body.classList.add("body--smile");
       } else {
         output.innerHTML += `
             <h2>${congratsMessage25}</h2>
@@ -58,10 +65,9 @@ checkBtn.addEventListener("click", (event) => {
       let loss = totalCP - totalSP;
       let lossPercent = (loss / totalCP) * 100;
 
-      let consolationMessage25 = "Maybe today's not your day !";
-      let consolationMessage50 = "Woah this is brutal !";
-      let consolationMessage75 =
-        "May have to completely rethink this investment.";
+      let consolationMessage25 = `Maybe today's not your day <i class="fas fa-tired emoji"></i>`;
+      let consolationMessage50 = `Woah this is brutal <i class="fas fa-sad-tear emoji"></i>`;
+      let consolationMessage75 = `May have to completely rethink this investment <i class="fas fa-angry emoji"></i>`;
 
       output.innerHTML = `
             <h2>Oh you made a loss of ${lossPercent}%, which sums up to a total of ₹${loss}.</h2>
@@ -70,10 +76,12 @@ checkBtn.addEventListener("click", (event) => {
         output.innerHTML += `
             <h2>${consolationMessage75}</h2>
             `;
+        document.body.classList.add("body--danger");
       } else if (lossPercent >= 50) {
         output.innerHTML += `
-            <h2>${consolationMessage50}</h2>
-            `;
+        <h2>${consolationMessage50}</h2>
+        `;
+        document.body.classList.add("body--warning");
       } else {
         output.innerHTML += `
             <h2>${consolationMessage25}</h2>
@@ -84,6 +92,7 @@ checkBtn.addEventListener("click", (event) => {
       output.innerHTML = `
         <h2>Neither profit nor loss. This one looks fine.</h2>
         `;
+      document.body.classList.add("body--no-feelings");
     }
   }
   document.body.appendChild(output);
